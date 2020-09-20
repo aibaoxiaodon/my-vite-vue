3e5dd75c2fefe1a9172157fa903aa9aba817986c
aibaoxiaodon <sxczhxd@163.com>
2020-09-20 16:43:52 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ title }}, 第一个vue3的项目</h1>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const title = ref('Hello, word')
+    return {
+      title
+    }
+  },
+  name: 'App'
+}
+</script>
